6ac0bbced98f0099cf92bf4838a96164c2718bca
iam-shiva <117497491+dsivaa77@users.noreply.github.com>
2024-05-15 22:03:56 +0530

    Update MydiaryApplicationTests.java

--- src/test/java/com/spring/mydiary/MydiaryApplicationTests.java
+++ src/test/java/com/spring/mydiary/MydiaryApplicationTests.java
@@ -2,7 +2,7 @@ package com.spring.mydiary;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+System.out.println("satya");
 @SpringBootTest
 class MydiaryApplicationTests {
 
